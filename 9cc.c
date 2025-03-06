84e1ee0943199081d693be4f8a60f2ad11c82d94
harumaki <115347054+Amoonguses1@users.noreply.github.com>
2025-03-06 11:47:15 +0900

    Add addition and subtraction.

--- 9cc.c
+++ 9cc.c
@@ -8,10 +8,32 @@ int main(int argc, char **argv)
         fprintf(stderr, "引数の個数が正しくありません\n");
         return 1;
     }
+    char *p = argv[1];
 
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov x0, %d\n", atoi(argv[1]));
-    printf("  ret\n");
+    printf("    mov x0, %ld\n", strtol(p, &p, 10));
+
+    while (*p)
+    {
+        if (*p == '+')
+        {
+            p++;
+            printf("    add x0, x0, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-')
+        {
+            p++;
+            printf("    sub x0, x0, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "an unexpected character: %c\n", *p);
+        return 1;
+    }
+
+    printf("    ret \n");
     return 0;
 }
